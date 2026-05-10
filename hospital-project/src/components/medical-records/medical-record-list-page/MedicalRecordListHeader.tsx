4c5619d4d33cc3fe8404f7023ffa6a13799817cf
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-05-10 10:20:49 +0700

    feat(medical-records): add MedicalRecordListHeader component

---
+++ hospital-project/src/components/medical-records/medical-record-list-page/MedicalRecordListHeader.tsx
@@ -0,0 +1,28 @@
+﻿import { Button, Space, Typography } from "antd";
+import { PlusOutlined, ReloadOutlined } from "@ant-design/icons";
+
+interface Props {
+  onOpenCreate: () => void;
+  onReload: () => void;
+}
+
+const MedicalRecordListHeader = ({ onOpenCreate, onReload }: Props) => {
+  return (
+    <Space style={{ width: "100%", justifyContent: "space-between" }}>
+      <Typography.Title level={4} style={{ margin: 0 }}>
+        Quản lý Hồ Sơ Bệnh Án
+      </Typography.Title>
+
+      <Space>
+        <Button icon={<PlusOutlined />} type="primary" onClick={onOpenCreate}>
+          Tạo hồ sơ mới
+        </Button>
+        <Button icon={<ReloadOutlined />} onClick={onReload}>
+          Tải lại
+        </Button>
+      </Space>
+    </Space>
+  );
+};
+
+export default MedicalRecordListHeader;
