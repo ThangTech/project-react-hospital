48622c3032268bf4ed6c383848ef615066eb5635
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2026-05-14 12:33:00 +0700

    Load home data

--- hospital-project/src/pages/HomePage.tsx
+++ hospital-project/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import HeroSlider from "../components/home/HeroSlider";
 import StatsStrip from "../components/home/StatsStrip";
 import AboutSection from "../components/home/AboutSection";
@@ -5,21 +6,35 @@ import ServicesSection from "../components/home/ServicesSection";
 import FaqSection from "../components/home/FaqSection";
 import DoctorsSection from "../components/home/DoctorsSection";
 import BlogPreviewSection from "../components/home/BlogPreviewSection";
-import type { KhoaPhong, BacSi } from "../types";
+import { getAllDoctors } from "../services/api.doctor.service";
+import { getAllDepartments } from "../services/api.bed-department.service";
+import type { BacSi, KhoaPhong } from "../types";
 
-const departments: KhoaPhong[] = [];
-const doctors: BacSi[] = [];
+const HomePage = () => {
+  const [departments, setDepartments] = useState<KhoaPhong[]>([]);
+  const [doctors, setDoctors] = useState<BacSi[]>([]);
 
-const HomePage = () => (
-  <div>
-    <HeroSlider />
-    <StatsStrip />
-    <AboutSection />
-    <ServicesSection departments={departments} />
-    <FaqSection />
-    <DoctorsSection doctors={doctors} />
-    <BlogPreviewSection />
-  </div>
-);
+  const loadData = async () => {
+    const [deptList, doctorList] = await Promise.all([getAllDepartments(), getAllDoctors()]);
+    setDepartments(deptList);
+    setDoctors(doctorList);
+  };
+
+  useEffect(() => {
+    void loadData();
+  }, []);
+
+  return (
+    <div>
+      <HeroSlider />
+      <StatsStrip />
+      <AboutSection />
+      <ServicesSection departments={departments} />
+      <FaqSection />
+      <DoctorsSection doctors={doctors} />
+      <BlogPreviewSection />
+    </div>
+  );
+};
 
 export default HomePage;
